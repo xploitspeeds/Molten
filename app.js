7a9ecd86fdcccff6c956bb23e2636dfcec4fd678
Ryan Wilson <45722321+deopdeop@users.noreply.github.com>
2021-04-13 15:37:59 -0400

    Updated code to allow for continued contributing

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ module.exports = class {
             this.baseUrl = new URL((req.connection.encrypted ? 'https' : !req.connection.encrypted ? 'http' : null) + '://' + req.headers.host),
             this.clientUrl = new URL(req.url.slice(this.prefix.length));
         } catch (err) {
-            if (typeof err == TypeError) return req.writeHead(500, { 'content-type': 'text/plain' })
-                .end(err);
+            typeof err == TypeError && 
+                req
+                    .writeHead(500, { 'content-type': 'text/plain' })
+                    .end(err);
         }
 
         const rewriter = new Rewriter({
@@ -28,43 +30,56 @@ module.exports = class {
                 clientUrl: this.clientUrl
             }), 
             client = https.request(
-                this.clientUrl.href,
+                this.clientUrl.href, 
                 { 
                     headers: Object.entries(req.headers).map(([header, directives]) => rewriter.header([header, directives])).filter(map => map),
                     method: req.method,
                     followAllRedirects: false 
                 }, 
-                (clientResp, streamData = [], sendData = '') => clientResp.on('data', data => streamData.push(data))
-                    .on('end', () => {
-                        const enc = clientResp.headers['content-encoding'],
-                            type = clientResp.headers['content-type'];
+                (clientResp, streamData = [], sendData = '') => 
+                    clientResp
+                        .on('data', data => streamData.push(data))
+                        .on('end', () => {
+                            const enc = clientResp.headers['content-encoding'],
+                                type = clientResp.headers['content-type'];
 
-                        typeof enc != 'undefined' ? enc.split`; `[0].split`, `.forEach(encType => 
-                            sendData = encType == 'gzip' ? 
-                            zlib.gunzipSync(Buffer.concat(streamData)).toString() : 
-                            encType == 'deflate' ? zlib.inflateSync(Buffer.concat(streamData)).toString() : 
-                            encType == 'br' ? zlib.brotliDecompressSync(Buffer.concat(streamData)).toString() : 
-                            null 
-                        ) :
-                        sendData = Buffer.concat(streamData).toString();
+                            typeof enc != 'undefined' 
+                                ? enc.split`; `[0].split`, `.forEach(encType => sendData = 
+                                    encType == 'gzip'
+                                        ? zlib.gunzipSync(Buffer.concat(streamData))
+                                            .toString() 
+                                    : encType == 'deflate'
+                                        ? zlib.inflateSync(Buffer.concat(streamData))
+                                            .toString()
+                                    : encType == 'br' 
+                                        ? zlib.brotliDecompressSync(Buffer.concat(streamData))
+                                            .toString()
+                                    : null 
+                                )
+                                : sendData = Buffer.concat(streamData).toString();
 
-                        if (typeof type != 'undefined') {
-                            const directive = type.split`;`[0];
+                            typeof type != 'undefined' && (
+                                directive = type.split`;`[0],
+                                sendData = 
+                                    directive == 'text/html'
+                                        ? rewriter.html(sendData)
+                                    : directive == 'text/css'
+                                        ? rewriter.css(sendData)
+                                    : ['text/javascript', 'application/x-javascript', 'application/javascript'].includes(directive)
+                                        ? rewriter.js(sendData)
+                                    : sendData
+                            );
 
-                            sendData = directive == 'text/html' ? rewriter.html(sendData) : 
-                            directive == 'text/css' ? rewriter.css(sendData) :
-                            ['text/javascript', 'application/x-javascript', 'application/javascript'].includes(directive) ? rewriter.js(sendData) : 
-                            sendData;
-                        };
-
-                        resp.writeHead(clientResp.statusCode, Object.entries(clientResp.headers).map(([header, directives]) => rewriter.header([header, directives])).filter(map => map))
-                            .end(sendData);
-                    })
+                            resp
+                                .writeHead(clientResp.statusCode, Object.entries(clientResp.headers).map(([header, directives]) => rewriter.header([header, directives])).filter(map => map))
+                                .end(sendData);
+                        })
             );
                 
         client.on('error', err => resp.end(err.message));
         
-        req.on('data', data => client.write(data))
+        req
+            .on('data', data => client.write(data))
             .on('end', () => client.end());
     };
 
@@ -78,7 +93,8 @@ module.exports = class {
 
             const sendReq = new WebSocket(clientUrl.href, { headers: req.headers }).on('message', msg => client.send(msg))
                 .on('open', () => sendReq.send(msgParts))
-                .on('error', () => client.end()).on('close', () => client.close());
+                .on('error', () => client.end())
+                .on('close', () => client.close());
 
             client.on('message', msg => sendReq.readyState == WebSocket.open ? sendReq.send(msg) : msgParts.push(msg))
                 .on('error', () => sendReq.end())
--- rewriter.js
+++ rewriter.js
@@ -1,6 +1,9 @@
-const nodejs = typeof exports !== 'undefined' && this.exports !== exports, 
-    url = nodejs ? require('url') : null,
-    util = nodejs ? require('util') : null;
+const nodejs = typeof exports !== 'undefined' && this.exports !== exports;
+
+nodejs && (
+    url = require('url'),
+    util = require('url') 
+);
 
 module.exports = class {
     constructor(passthrough = {}) {
@@ -17,125 +20,130 @@ module.exports = class {
             directives
                 .join``
                 .split`; `
-                .map(directive => {
-                    pair = directive.split`=`;
-
-                    if (pair.length == 2) {
-                        pair[1] = pair[0] == 'domain' ? this.baseUrl.hostname :
+                .map(directive => (
+                    pair = directive.split`=`,
+                    pair.length == 2
+                        ? pair[1] = pair[0] == 'domain' ? this.baseUrl.hostname :
                         pair[0] == 'path' ? this.prefix + pair[1] :
-                        pair[1];
-
-                        return pair.join`=` + pair[1].replace(/=/g, '&equiv;');
-                    }
-                    else return directive;
-                })
+                        pair[1]
+                        : pair.join`=` + pair[1].replace(/=/g, '&equiv;')
+                ))
                 .join``
     };
 
-    header([header, directives]) {
-        // TODO: Map header to object
-        this.csp = header == 'content-security-policy' ? null : null,
-        this.tao = header == 'timing-allow-origin' ? null : null;
-
-        return ['content-encoding', 'content-length', 'content-security-policy', 'timing-allow-origin', 'transfer-encoding', 'referrer-policy', 'x-frame-options'].includes(header) ? null :
-        header == 'host' ? [header, this.clientUrl.host] :
-        ['cookie', 'cookie2'].includes(header) ? [header, this.cookie.get(directives)] :
-        header == 'location' ? [header, this.prefix + directives] :
-        header == 'referrer' ? [header, directives.slice(this.prefix.length)] :
-        ['set-cookie', 'set-cookie2'].includes(header) ? [header, this.cookie.set(directives)] :
-        [header, directives];
-    }
+    header = ([header, directives]) => (
+        this.csp = header == 'content-security-policy' 
+            ? null
+            : null,
+        this.tao = header == 'timing-allow-origin' 
+            ? null 
+            : null,
+        ['content-encoding', 'content-length', 'content-security-policy', 'timing-allow-origin', 'transfer-encoding', 'referrer-policy', 'x-frame-options'].includes(header) 
+            ? null
+        : header == 'host' 
+            ? [header, this.clientUrl.host] 
+        : ['cookie', 'cookie2'].includes(header)
+            ? [header, this.cookie.get(directives)]
+        : header == 'location' 
+            ? [header, this.prefix + directives]
+        : header == 'referrer' 
+            ? [header, directives.slice(this.prefix.length)] 
+        : ['set-cookie', 'set-cookie2'].includes(header)
+            ? [header, this.cookie.set(directives)] 
+        : [header, directives]
+    );
 
     // TODO: Rewrite URLS
     url = url => null;
 
     // TODO: Emulate security policies
     attr = ([attr, src]) => 
-        ['action', 'data', 'href', 'poster', 'src', 'xlink:href'].includes(attr) ? [attr, this.url(src)] :
-        ['integrity', 'nonce'].includes(attr) ? null :
-        attr.startsWith`on-` ? this.js(src) :
-        attr == 'style' ? [attr, this.css(src)] :
-        attr == 'srcdoc' ? [attr, this.html(src)] :
-        attr == 'srcset' ?  [attr, src.split` `.map((src, i) => !(i % 2) ? this.url(src) : src).join` `] :
-        [attr, value];
+        ['action', 'data', 'href', 'poster', 'src', 'xlink:href'].includes(attr) 
+            ? [attr, this.url(src)]
+        : ['integrity', 'nonce'].includes(attr) 
+            ? null
+        : attr.startsWith`on-` 
+            ? this.js(src) 
+        :
+        attr == 'style' 
+            ? [attr, this.css(src)]
+        : attr == 'srcdoc' 
+            ? [attr, this.html(src)]
+        : attr == 'srcset' 
+            ? [attr, src.split` `.map((src, i) => !(i % 2) ? this.url(src) : src).join` `] 
+        : [attr, value];
 
-    // I have decided I should use parse5 instead
-    html(body) {
-        // TODO: Use parse5 instead
-        const jsdom = nodejs ? require('jsdom').JSDOM : null, 
-            fs = nodejs ? require('fs') : null, 
-            dom = nodejs ? new jsdom(body, { contentType: 'text/html'}) : new DOMParser.parseFromString(body, 'text/html'),
-            inject = dom.window.document.createElement`SCRIPT`;
-    
-        dom.window.document.querySelectorAll`*`.forEach(elm => {
-            // BUG: Doing this will break the DOM structure
-            /*
-            elm.textContent = elm.tagName == 'SCRIPT' ? this.js(elm.textContent) :
-            elm.tagName == 'STYLE' ? this.css(elm.textContent) :
-            body;
-            */
+    html = body => 
+        nodejs 
+            ? (
+                parse5 = require('parse5'),
+                dom = parse5.parse(body),
+                dom.childNodes.forEach(node => rewriter.attr([node.name, node.value])),
+                parse5.serialize(dom)
+            )
+            : (
+                dom = new DOMParser.parseFromString(body, 'text/html'),
+                dom.querySelectorAll`*`.forEach(elm => (
+                    elm.textContent = 
+                        elm.tagName == 'SCRIPT' 
+                            ? this.js(elm.textContent) 
+                        : elm.tagName == 'STYLE'
+                            ? this.css(elm.textContent)
+                        : elm.textContent,
+                    elm.getAttributeNames().forEach(name => elm.setAttribute(...this.attr([name, elm.getAttribute(name)])))
+                )),
+                dom.querySelector`*`.outerHTML
+            );
 
-            elm.getAttributeNames().forEach(name => elm.setAttribute(...this.attr([name, elm.getAttribute(name)])));
-        });
+    css = body => 
+        nodejs
+            ? (
+                csstree = require('css-tree'),
+                ast = csstree.parse(body),
+                csstree.walk(ast, node => node.type == 'Url' && this.url(node.value)),
+                csstree.generate(ast)
+            )
+            : (
+                dom = new DOMParser.parseFromString(`<style>${body}</style>`, 'text/html'),
+                dom.getElementsByTagName`style`.innerHTML
+            )
 
-        nodejs && (
-            prefix = 'INSERT',
-            inject.textContent = fs.readFileSync('rewriter.js', 'utf8')
-                .replaceAll('module.exports', 'Rewriter')
-                .replaceAll(prefix + 'PREFIX', this.prefix)
-                .replaceAll(prefix + 'BASE_URL', util.inspect(this.baseUrl))
-                .replaceAll(prefix + 'CLIENT_URL', util.inspect(this.clientUrl))
-                .replaceAll(prefix + 'CORS', util.inspect(this.cors))
-                .replaceAll(prefix + 'TAO', util.inspect(this.tao))
-                .replaceAll(prefix + 'ORIGINAL_COOKIE', escape(this.originalCookie))
-                .replaceAll(prefix + 'DOM', escape(body)),
-            // If a meta tag with charset attribute is present move it to the top of the head tag and prepend
-            dom.window.document.getElementsByTagName`HEAD`[0].prepend(inject)
-        );
-        
-        return dom.window.document.querySelector`*`.outerHTML;
-    }
-
-    // Parse css with the help of domparser object - CSSWG compliant
-    // I will post the urls to both the CSSWG specification, DOMParser, Parse5, and the style properties
-    css = body => body.replaceAll(/(?<=url\((?<a>["']?)).*?(?=\k<a>\))|(?<=@import *(?<b>"|')).*?(?=\k<b>.*?;)/g, url => this.url(url));
-
-    js = body => '{document=proxifiedDocument;' + body + '}';
+    js = body => `{document=proxifiedDocument;${body}`;
 };
 
-if (!nodejs) {
-    const passthrough = {
-            prefix: 'INSERT_PREFIX', 
-            baseUrl: INSERT_BASE_URL, 
-            clientUrl: INSERT_CLIENT_URL, 
-            security: {
-                cors: INSERT_CORS,
-                tao: INSERT_TAO
-            }, 
-            original: {
-                dom: unescape`INSERT_DOM`
-            }
-        },
-        rewriter = new Rewriter({
-            prefix: passthrough.prefix, 
-            baseUrl: passthrough.baseUrl, 
-            clientUrl: passthrough.clientUrl
-        }),
-        htmlHandler = {
-            apply(target, thisArg, args) {
-                args[0] = rewriter.html(args[0]);
+!nodejs && (
+    passthrough = {
+        prefix: 'INSERT_PREFIX', 
+        baseUrl: INSERT_BASE_URL, 
+        clientUrl: INSERT_CLIENT_URL, 
+        security: {
+            cors: INSERT_CORS,
+            tao: INSERT_TAO
+        }, 
+        original: {
+            cookie: unescape`INSERT_COOKIE`,
+            dom: unescape`INSERT_DOM`
+        }
+    },
+    rewriter = new Rewriter({
+        prefix: passthrough.prefix, 
+        baseUrl: passthrough.baseUrl, 
+        clientUrl: passthrough.clientUrl
+    }),
+    htmlHandler = {
+        apply(target, thisArg, args) {
+            args[0] = rewriter.html(args[0]);
     
-                return Reflect.apply(target, thisArg, args);
-            }
-        },
-        historyHandler = {
-            apply(target, thisArg, args) {
-                args[2] = rewriter.url(args[2]);
-    
-                return Reflect.apply(target, thisArg, args);
-            }
-        };
+            return Reflect.apply(target, thisArg, args);
+        }
+    },
+    historyHandler = {
+        apply(target, thisArg, args) {
+            args[2] = rewriter.url(args[2]);
 
+            return Reflect.apply(target, thisArg, args);
+        }
+    },
     element
         .prototype
             .innerHTML = new Proxy(element.prototype.innerHTML, htmlHandler)
@@ -146,27 +154,15 @@ if (!nodejs) {
     
                 return Reflect.apply(target, thisArg, args);
             }
-        });
-    
-    // TODO: Instead of rewriting properties that modify and read the dom individually figure out where the DOM tree is actually stored and referenced and proxify the data theres
-    // I hope this isn't hidden
+        }),
     window.proxifiedDocument = new Proxy(document, {
         get(target, prop) {
-            target[prop] = 
-                prop == 'cookie' ? rewriter.cookie.get(target[prop]) :
-                prop == 'location' ? passthrough.clientUrl.href :
-                typeof(prop = Reflect.get(target, prop)) == 'function' ? prop.bind(target) : 
-                target[prop];
+            target[prop] = null;
 
             return Reflect.get(target, prop);
         },
-
         set(target, prop) {
-            target[prop] = 
-                prop == 'cookie' ? rewriter.cookie.set(target[prop]) :
-                prop == 'location' ? new URL(this.baseUrl.protocol + '//' + this.baseUrl.hostname + this.prefix + (target[prop]).href) :
-                prop == 'cookie' ? rewriter.cookie.set(target[prop]) :
-                target[prop];
+            target[prop] = null;
 
             return Reflect.set(target, prop);
         }
@@ -190,14 +186,14 @@ if (!nodejs) {
         })
         .open = new Proxy(window.open, {
             apply(target, thisArg, args) {
-                args[0] = passthrough.httpPrefix + args[0];
+                args[0] = null;
 
                 return Reflect.apply(target, thisArg, args);
             }
         })
         .postMessage = new Proxy(window.postMessage, {
             apply(target, thisArg, args) {
-                args[1] = passthrough.baseUrl.href;
+                args[1] = null;
 
                 return Reflect.apply(target, thisArg, args);
             }
@@ -206,7 +202,7 @@ if (!nodejs) {
             construct(target, args) {
                 const url = new URL(args[0]);
 
-                args[0] = url.protocol + '//' + passthrough.baseUrl.host + passthrough.prefix + url.href;
+                args[0] = null;
                 
                 return Reflect.construct(target, args);
             }
@@ -225,20 +221,18 @@ if (!nodejs) {
 
                 return Reflect.apply(target, thisArg, args);
             }
-        });
-
-    delete window.MediaStreamTrack; 
-    delete window.RTCPeerConnection;
-    delete window.RTCSessionDescription;
-    delete window.mozMediaStreamTrack;
-    delete window.mozRTCPeerConnection;
-    delete window.mozRTCSessionDescription;
-    delete window.navigator.getUserMedia;
-    delete window.navigator.mozGetUserMedia;
-    delete window.navigator.webkitGetUserMedia;
-    delete window.webkitMediaStreamTrack;
-    delete window.webkitRTCPeerConnection;
-    delete window.webkitRTCSessionDescription;
-
-    document.currentScript.remove();
-}
+        }),
+    delete window.MediaStreamTrack, 
+    delete window.RTCPeerConnection,
+    delete window.RTCSessionDescription,
+    delete window.mozMediaStreamTrack,
+    delete window.mozRTCPeerConnection,
+    delete window.mozRTCSessionDescription,
+    delete window.navigator.getUserMedia,
+    delete window.navigator.mozGetUserMedia,
+    delete window.navigator.webkitGetUserMedia,
+    delete window.webkitMediaStreamTrack,
+    delete window.webkitRTCPeerConnection,
+    delete window.webkitRTCSessionDescription,
+    document.currentScript.remove()
+)
